2ef786f1f169818aae8dbdc91814aaaccf19899c
Jan Robert <janmartinrobert@gmail.com>
2020-05-06 11:15:41 +0200

    Add correct match string

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders hello world', () => {
   const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
+  const linkElement = getByText(/Hello World/i)
   expect(linkElement).toBeInTheDocument()
 })
