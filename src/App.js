be1d0fe657c501ad6defd6a07eefccfa09094153
albert-schilling <albertschilling@gmx.net>
2020-05-07 11:15:53 +0200

    Add react router package and routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Suspense, lazy } from 'react'
 import styled from 'styled-components/macro'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Home from './components/Home/Home'
 
 const Details = lazy(() => import('./components/Details/Details'))
@@ -8,13 +9,21 @@ const About = lazy(() => import('./components/About/About'))
 export default function App() {
   return (
     <AppStyled>
-      <Home />
-      <Suspense fallback={<p>Loading ...</p>}>
-        <Details />
-      </Suspense>
-      <Suspense fallback={<p>Loading ...</p>}>
-        <About />
-      </Suspense>
+      <Router>
+        <Switch>
+          <Route to="/">
+            <Home />
+          </Route>
+          <Suspense fallback={<p>Loading ...</p>}>
+            <Route to="/details">
+              <Details />
+            </Route>
+            <Route to="/about">
+              <About />
+            </Route>
+          </Suspense>
+        </Switch>
+      </Router>
     </AppStyled>
   )
 }
